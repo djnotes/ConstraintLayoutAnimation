4f3eb9989fbc87794f3b11eaddb3754e04d703f7
Mehdi Haghgoo <mehdihaghgoo2014@gmail.com>
2018-01-11 14:40:08 +0330

    Added tension to transition

--- app/src/main/java/mehdi/constimation/MainActivity.java
+++ app/src/main/java/mehdi/constimation/MainActivity.java
@@ -8,9 +8,13 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
 import android.transition.TransitionManager;
+import android.transition.TransitionValues;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,10 +24,10 @@ public class MainActivity extends AppCompatActivity{
     private Button mButton; //Start button
     private ConstraintSet mConstraintSet;
     private ConstraintLayout mConstraintLayout; //Our root layout
-    private TextView mTitleTV;
     private Context mContext = this;
     private static final int [] mLayouts = {R.layout.summary, R.layout.detail};
     public static int mArrayIndex = 0; //This keeps track of selected layout
+    private TextView mTitleTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +39,15 @@ public class MainActivity extends AppCompatActivity{
 
         mButton = findViewById(R.id.start);
 
+        mTitleTV = findViewById(R.id.title);
+
         //Load a ConstraintSet and load it from our second layout
         mConstraintSet = new ConstraintSet();
-
-        mTitleTV = findViewById(R.id.title);
 
 
 
         mButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 //change layouts
@@ -52,8 +56,14 @@ public class MainActivity extends AppCompatActivity{
 
                 mConstraintSet.applyTo(mConstraintLayout);
 
+                //Use a different interpolator here
+                Transition transition = new ChangeBounds(mContext, null);
+
+                //Add tension to the animation
+                transition.setInterpolator( new AnticipateOvershootInterpolator(1.0f));
                 //Show to animation
-                TransitionManager.beginDelayedTransition(mConstraintLayout);
+                TransitionManager.beginDelayedTransition(mConstraintLayout, transition);
+
             }
         });
 
